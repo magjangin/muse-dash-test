0adf64066faf34599f4da7d94c073bc51b056e5d
magjangin <magjangin@users.noreply.github.com>
2026-05-24 11:38:10 +0900

    Add RefreshTxt and InitMusicCell hooks and clean up accessor patches

--- muse dash test/Patches/UI/PnlStagePatch.cs
+++ muse dash test/Patches/UI/PnlStagePatch.cs
@@ -634,3 +634,65 @@ public static class PnlStagePatchHelper
         }
     }
 }
+
+// MusicButtonAreaTitle.RefreshTxt 후킹
+[HarmonyLib.HarmonyPatch(typeof(Il2Cpp.MusicButtonAreaTitle), "RefreshTxt", new Type[] { typeof(string), typeof(bool) })]
+public class MusicButtonAreaTitle_RefreshTxt_Patch
+{
+    public static bool Prepare()
+    {
+        MelonLogger.Msg("[MusicButtonAreaTitle.RefreshTxt] 후킹 준비 완료");
+        return true;
+    }
+
+    public static void Prefix(Il2Cpp.MusicButtonAreaTitle __instance, ref string title, ref bool isSpecialFont)
+    {
+        try
+        {
+            if (__instance != null)
+            {
+                string gameObjectName = __instance.gameObject != null ? __instance.gameObject.name : "(null)";
+                MelonLogger.Msg($"[MusicButtonAreaTitle.RefreshTxt] Prefix 호출됨! GameObject={gameObjectName} | 원본 title='{title}' | isSpecialFont={isSpecialFont}");
+            }
+        }
+        catch (Exception ex)
+        {
+            MelonLogger.Error($"MusicButtonAreaTitle.RefreshTxt Prefix 예외: {ex}");
+        }
+    }
+}
+
+// MusicButtonCell.InitMusicCell 후킹
+[HarmonyLib.HarmonyPatch(typeof(Il2CppAssets.Scripts.UI.Panels.PnlMusicTag.MusicButtonCell), "InitMusicCell", new Type[] { typeof(Il2CppAssets.Scripts.Database.MusicInfo), typeof(int) })]
+public class MusicButtonCell_InitMusicCell_Patch
+{
+    public static bool Prepare()
+    {
+        MelonLogger.Msg("[MusicButtonCell.InitMusicCell] 후킹 준비 완료");
+        return true;
+    }
+
+    public static void Prefix(Il2CppAssets.Scripts.UI.Panels.PnlMusicTag.MusicButtonCell __instance, ref Il2CppAssets.Scripts.Database.MusicInfo initMusicInfo, ref int tabIndex)
+    {
+        try
+        {
+            if (__instance != null && initMusicInfo != null)
+            {
+                string musicUid = initMusicInfo.uid;
+                string musicName = initMusicInfo.name;
+                string musicAuthor = initMusicInfo.author;
+                string gameObjectName = __instance.gameObject != null ? __instance.gameObject.name : "(null)";
+                
+                MelonLogger.Msg($"[MusicButtonCell.InitMusicCell] Prefix: GameObject={gameObjectName} | Uid={musicUid} | Name={musicName} | Author={musicAuthor} | TabIndex={tabIndex}");
+            }
+        }
+        catch (Exception ex)
+        {
+            MelonLogger.Error($"MusicButtonCell.InitMusicCell Prefix 예외: {ex}");
+        }
+    }
+}
+
+
+
+
