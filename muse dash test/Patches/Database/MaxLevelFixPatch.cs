02a7eea9a0e400ecbd1ded5c369ae4390be4db3f
magjangin <magjangin@users.noreply.github.com>
2026-07-22 12:52:08 +0900

    revert: remove 9999 level override patches while keeping level-up popup and sound blocking

--- muse dash test/Patches/Database/MaxLevelFixPatch.cs
+++ muse dash test/Patches/Database/MaxLevelFixPatch.cs
@@ -1,43 +1,14 @@
 using System;
 using HarmonyLib;
 using Il2Cpp;
-using Il2CppAssets.Scripts.Database;
-using Il2CppAssets.Scripts.UI;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
 
 namespace muse_dash_test.Patches.Database
 {
     /// <summary>
-    /// 글로벌/로컬 레벨 및 경험치 계산, 레벨업 검증 메서드를 정밀 지정하여
-    /// 표시 레벨을 9999(만렙)로 고정하고 레벨업 팝업 및 효과음("띠리링~")을 완벽 차단합니다.
+    /// 레벨 수치(9999 조작)는 변경하지 않고,
+    /// 곡 클리어 후 발생하는 레벨업 보상 팝업(PnlLevelUpAward) 및 효과음 연출("띠리링~")만 깔끔하게 차단합니다.
     /// </summary>
-    [HarmonyPatch(typeof(DataHelper), nameof(DataHelper.Level), MethodType.Getter)]
-    public static class DataHelper_Level_Patch
-    {
-        public static void Postfix(ref int __result)
-        {
-            __result = 9999;
-        }
-    }
-
-    [HarmonyPatch(typeof(AccountSaveUtils), nameof(AccountSaveUtils.CacularLevel))]
-    public static class AccountSaveUtils_CacularLevel_Patch
-    {
-        public static void Postfix(ref int __result)
-        {
-            __result = 9999;
-        }
-    }
-
-    [HarmonyPatch(typeof(AccountSaveUtils), nameof(AccountSaveUtils.CacularCurExp))]
-    public static class AccountSaveUtils_CacularCurExp_Patch
-    {
-        public static void Postfix(ref int __result)
-        {
-            __result = 999999;
-        }
-    }
-
     [HarmonyPatch(typeof(PnlUnlock), "CheckHaveEnoughExpToLevelUp")]
     public static class PnlUnlock_CheckHaveEnoughExpToLevelUp_Patch
     {
