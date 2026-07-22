ec512eb8d92f5a779c9712d9cea4d2be6f300ee1
magjangin <magjangin@users.noreply.github.com>
2026-07-22 12:43:14 +0900

    feat: patch AccountSaveUtils.CacularLevel to lock local level calculation to 9999

--- muse dash test/Patches/Database/MaxLevelFixPatch.cs
+++ muse dash test/Patches/Database/MaxLevelFixPatch.cs
@@ -1,14 +1,25 @@
 using HarmonyLib;
 using Il2CppAssets.Scripts.Database;
+using Il2CppAssets.Scripts.UI;
 
 namespace muse_dash_test.Patches.Database
 {
     /// <summary>
-    /// 계정 레벨을 Lv.9999 만렙으로 항시 고정하여
-    /// 곡 클리어 시 반복되는 레벨업 팝업(PnlLevelUpAward) 및 효과음 연출을 완전 차단합니다.
+    /// 로컬 EXP 기반 레벨 계산 함수(AccountSaveUtils.CacularLevel)와
+    /// DataHelper.Level 프로퍼티를 동시에 9999(만렙)로 고정하여
+    /// 로컬 데이터 및 UI 레벨 표기를 9999로 변경하고 레벨업 연출/효과음을 완벽히 차단합니다.
     /// </summary>
     [HarmonyPatch(typeof(DataHelper), nameof(DataHelper.Level), MethodType.Getter)]
-    public static class MaxLevelFixPatch
+    public static class DataHelper_Level_Patch
+    {
+        public static void Postfix(ref int __result)
+        {
+            __result = 9999;
+        }
+    }
+
+    [HarmonyPatch(typeof(AccountSaveUtils), nameof(AccountSaveUtils.CacularLevel))]
+    public static class AccountSaveUtils_CacularLevel_Patch
     {
         public static void Postfix(ref int __result)
         {
