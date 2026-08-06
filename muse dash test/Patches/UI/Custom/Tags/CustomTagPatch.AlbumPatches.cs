43166c4ad8ad572386a1f5670d37c2558e21fbbd
magjangin <magjangin@users.noreply.github.com>
2026-08-07 08:42:34 +0900

    feat: add LocalALBUMInfo localization patches (MusicInfo.GetLocal & DBConfigLocalALBUM)

--- muse dash test/Patches/UI/Custom/Tags/CustomTagPatch.AlbumPatches.cs
+++ muse dash test/Patches/UI/Custom/Tags/CustomTagPatch.AlbumPatches.cs
@@ -1,21 +1,48 @@
+using MelonLoader;
 using HarmonyLib;
+using Il2Cpp;
 using Il2CppAssets.Scripts.Database;
 using Il2CppAssets.Scripts.Database.DataClass;
-using MelonLoader;
+using Il2CppAssets.Scripts.PeroTools.Commons;
+using Il2CppAssets.Scripts.PeroTools.Managers;
+using System;
 using System.Collections.Generic;
 
 namespace muse_dash_test
 {
     internal partial class CustomTagPatch
     {
-        [HarmonyPatch(typeof(DBConfigAlbums), nameof(DBConfigAlbums.GetAlbumInfoByUid))]
-        internal class DBConfigAlbums_GetAlbumInfoByUid_Patch
+        /// <summary>
+        /// 태그 화면 로딩 시 발생할 수 있는 1000개 앨범 순회 병목 렉을 해결하는 성능 최적화 패치입니다.
+        /// </summary>
+        [HarmonyPatch(typeof(MusicTagManager), nameof(MusicTagManager.InitDatas))]
+        internal static class Fix1000AlbumsPatch
         {
-            private static bool Prefix(DBConfigAlbums __instance, string uid, ref AlbumInfo __result)
+            private static void Postfix()
             {
-                if (uid == CustomTagRegistry.AlbumUidString)
+                try
                 {
-                    __result = CustomTagRegistry.VirtualAlbumInfo;
+                    var configObject = Singleton<ConfigManager>.instance.GetConfigObject<DBConfigAlbums>();
+                    if (configObject != null)
+                    {
+                        configObject.m_MaxAlbumUid = configObject.count - 3;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MelonLogger.Warning($"m_MaxAlbumUid 성능 최적화 패치 적용 중 예외 발생 (비치명적): {ex.Message}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(DBConfigAlbums), nameof(DBConfigAlbums.GetAlbumInfoByMusicInfo))]
+        internal class DBConfigAlbums_GetAlbumInfoByMusicInfo_Patch
+        {
+            private static bool Prefix(DBConfigAlbums __instance, MusicInfo musicInfo, ref DBConfigAlbums.AlbumsInfo __result)
+            {
+                if (musicInfo != null && CustomContentIds.IsVirtualSong(musicInfo.uid) && CustomTagRegistry.CustomAlbumInfo != null)
+                {
+                    __result = CustomTagRegistry.CustomAlbumInfo;
                     return false;
                 }
 
@@ -23,14 +50,14 @@ namespace muse_dash_test
             }
         }
 
-        [HarmonyPatch(typeof(DBConfigAlbums), nameof(DBConfigAlbums.GetAlbumInfoByIndex))]
-        internal class DBConfigAlbums_GetAlbumInfoByIndex_Patch
+        [HarmonyPatch(typeof(DBConfigAlbums), nameof(DBConfigAlbums.GetAlbumsInfoByUid))]
+        internal class DBConfigAlbums_GetAlbumsInfoByUid_Patch
         {
-            private static bool Prefix(DBConfigAlbums __instance, int index, ref AlbumInfo __result)
+            private static bool Prefix(DBConfigAlbums __instance, string uid, ref DBConfigAlbums.AlbumsInfo __result)
             {
-                if (index == CustomTagRegistry.TagUid)
+                if (uid == CustomTagRegistry.AlbumUidString && CustomTagRegistry.CustomAlbumInfo != null)
                 {
-                    __result = CustomTagRegistry.VirtualAlbumInfo;
+                    __result = CustomTagRegistry.CustomAlbumInfo;
                     return false;
                 }
 
@@ -40,21 +67,6 @@ namespace muse_dash_test
 
         [HarmonyPatch(typeof(DBConfigAlbums), nameof(DBConfigAlbums.GetAlbumIndexByUid))]
         internal class DBConfigAlbums_GetAlbumIndexByUid_Patch
-        {
-            private static bool Prefix(DBConfigAlbums __instance, string uid, ref int __result)
-            {
-                if (uid == CustomTagRegistry.AlbumUidString)
-                {
-                    __result = CustomTagRegistry.TagUid;
-                    return false;
-                }
-
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(DBConfigAlbums), nameof(DBConfigAlbums.GetAlbumJsonIndexByUid))]
-        internal class DBConfigAlbums_GetAlbumJsonIndexByUid_Patch
         {
             private static bool Prefix(DBConfigAlbums __instance, string uid, ref int __result)
             {
